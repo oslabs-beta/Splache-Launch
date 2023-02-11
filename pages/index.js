87083d0179de9a3095fa664fd1c5f46e44ee4fd7
Nancy Zhang <zhangn356@gmail.com>
2023-02-11 11:36:37 -0500

    next-template

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+function HomePage() {
+  return (
+    <div>
+      <Head>
+        <title>Splache</title>
+      </Head>
+      <h1> Splache </h1>
+      <p>An Agile, Nimble, & Lightweight Solution to GraphQL Caching</p>
+    </div>
+  )
+}
+
+export default HomePage
