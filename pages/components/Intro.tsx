9cf63f58537a626369bb6ca9182cc43b1d8ca206
Nicholas Cathcart <nicholascathcart@Nicholass-MacBook-Air.local>
2023-02-16 10:11:02 -0500

    pagifying scroll done. Started to migrate towards mobile first css.

--- pages/components/Intro.tsx
+++ pages/components/Intro.tsx
@@ -1,16 +1,13 @@
 export function Intro () {
     return (
         <div className='view' id='intro'>
-        <section id = 'textbox1'>
-          <p>
-          <b><span style ={{fontSize: '20px'}}>Introducing Splache</span></b> (/splæʃ/), an agile, user-friendly, and lightweight JavaScript library 
-          <br/>that efficiently caches GraphQL queries using the power of Redis. 
-          <br/>The Splache library is designed for improved performance and reduced load on your GraphQL server.
-          <br/>Through leveraging the speed and scalability of Redis, 
-          <br/>Splache is able to provide an efficient and unique solution for caching GraphQL queries! 
-          </p>
-          
-        </section>
+            <div className='introBox'>
+                <p>
+                <span style ={{fontSize: '24px', color: 'rgb(81, 136, 170)'}}>Introducing Splache (/splæʃ/),</span> an agile, user-friendly, and lightweight JavaScript library that efficiently caches GraphQL queries using the power of Redis. The Splache library is designed for improved performance and reduced load on your GraphQL server. Through leveraging the speed and scalability of Redis, Splache is able to provide an efficient and unique solution for caching GraphQL queries!
+                </p>
+            </div>
         </div>
     )
 }
+
+
